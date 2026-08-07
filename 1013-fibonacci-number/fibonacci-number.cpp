9b734ea766c6174551dc834a0c05381977250d06
Shivang Dixit <theshivangdixit2608@gmail.com>
2026-08-08 04:31:14 +0530

    Time: 3 ms (64.80%) | Memory: 8 MB (21.03%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n<=1)return n;
-        int prev2=0;
-        int prev1=1;
-        for(int i =2;i<=n;i++){
-            int current=prev2+prev1;
-            prev2=prev1;
-            prev1=current;
+        if (n <= 1) {
+            return n;
         }
-        return prev1;
+        vector<vector<long long>> result = {{1, 0}, {0, 1}};
+        vector<vector<long long>> base = {{1, 1}, {1, 0}};
+        int exp = n;
+        while (exp > 0) {
+            if (exp & 1) {
+                result = multiply(result, base);
+            }
+            base = multiply(base, base);
+            exp >>= 1;
+        }
+        return (int) result[0][1];
+    }
 
+    vector<vector<long long>> multiply(vector<vector<long long>>& a,
+                                       vector<vector<long long>>& b) {
+        return {
+            {a[0][0] * b[0][0] + a[0][1] * b[1][0],
+             a[0][0] * b[0][1] + a[0][1] * b[1][1]},
+            {a[1][0] * b[0][0] + a[1][1] * b[1][0],
+             a[1][0] * b[0][1] + a[1][1] * b[1][1]}
+        };
     }
 };
